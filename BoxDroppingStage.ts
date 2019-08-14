5e4d786b010b5966662c2a9aeb24735d4049e9fc
Anwesh43 <anweshthecool0@gmail.com>
2019-08-14 17:10:53 +0530

    adding ScaleUtil class

--- BoxDroppingStage.ts
+++ BoxDroppingStage.ts
@@ -18,3 +18,18 @@ class Stage {
         this.body.appendChild(component)
     }
 }
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static updateScale(scale : number, dir : number) : number {
+        return dir * scale * scGap
+    }
+}
